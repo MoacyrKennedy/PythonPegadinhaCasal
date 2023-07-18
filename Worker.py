1ba134dd4e254a626cef06b5047dea31d254c9ec
Moacyr kennedy <105392455+MoacyrKennedy@users.noreply.github.com>
2023-07-18 10:59:20 -0300

    Serviço

---
+++ Worker.py
@@ -0,0 +1,24 @@
+import tkinter
+from tkinter import messagebox
+
+root = tkinter.Tk()
+root.withdraw()
+
+msg_box = messagebox.showwarning("TOMO PAPUDO", "VOCÊ FOI HACKEADA")
+
+if msg_box:
+    msg_box = messagebox.showwarning("PERAI AI!", "PARA SER DESHACKEADA PRECISO QUE ME RESPONDA UMA PERGUNTA:")
+
+if msg_box:
+    msg_box = messagebox.askquestion("O QUE ACHA?", "ACEITA CASAR COMIGO")
+
+count = 1
+while msg_box == 'no':
+    count += 1
+    if count == 6:
+        msg_box = messagebox.showerror("TO INDO AI!", "SE VAI APANHAR FEIO")
+        break
+    msg_box = messagebox.askquestion("O QUE ACHA?", "ACEITA CASAR COMIGO")
+
+if msg_box == 'yes':
+    msg_box = messagebox.showinfo("Boa!", "SABIA ESCOLHA!")
